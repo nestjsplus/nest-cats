a0f899f96fcf407e9adef1c0e90fe6b8a835e931
John Biundo <johnfbiundo@gmail.com>
2019-08-24 17:40:41 -0700

    fix modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
-import { CatsController } from './cats/cats.controller';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CatsService } from './cats/cats.service';
+import { CatsModule } from './cats/cats.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, CatsController],
-  providers: [AppService, CatsService],
+  imports: [CatsModule],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
